bb9bf551de017c451bec839a03d449d03e72ce1e
Patrick Limp <patrickpereira15@gmail.com>
2025-04-07 11:12:41 -0300

    atualização

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-function sortearNumeros () {
-
+function sortearNumeros (){
     const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+
     const numeros = [];
 
     while (numeros.length < 6) {
-
-        const num = Math.floor(Math.random() * 60) +1;
+        const num = Math.floor(Math.random() * 6) + 1;
         if (!numeros.includes(num)) {
             numeros.push(num);
         }
     }
 
-    resultado.textContent = numeros.join(" - ")
+    numeros.forEach(numero => {
+        const bolinha = document.createElement("div");
+        bolinha.className = "numero";
+        bolinha.textContent = numero;
+        resultado.appendChild (bolinha);
+    })
 }
